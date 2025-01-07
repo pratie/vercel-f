6d706519c89073475894dc9664eea71618465ff4
prathapkr <your_email@example.com>
2025-01-07 22:52:01 +0530

    atl logo-object-cover

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -33,12 +33,12 @@ export function Navbar() {
               <span className="text-[#ff4500] text-2xl font-bold tracking-tight">
                 SNEAKYGUY
               </span>
-              <div className="relative h-13 w-13">
+              <div className="relative h-10 w-10">
                 <Image 
                   src="/logo.png"
                   alt="Sneakylogo"
                   fill
-                  className="object-contain"
+                  className="object-cover"
                 />
               </div>
             </div>
